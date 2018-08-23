c7707bf291ee07d61fdeb41e108ff19ecbcbc81f
Matias <mpierdominici@itba.edu.ar>
2018-08-23 14:37:07 -0300

    mediciones

---
+++ Ej1/Mediciones/bode_super.m
@@ -0,0 +1,17 @@
+clear all;
+close all;
+a=csvread('x01.csv');
+b=csvread('tst.csv');
+c=csvread('x10.csv');
+freca=a(:,1).*1000;
+frecb=b(:,1).*1000;
+frecc=c(:,1).*1000;
+gaina=20*log10(a(:,3)./a(:,2));
+gainb=20*log10(b(:,3)./b(:,2));
+gainc=20*log10(c(:,3)./c(:,2));
+
+semilogx(freca,gaina);
+hold on;
+semilogx(frecb,gainb);
+semilogx(frecc,gainc);
+hold off;
